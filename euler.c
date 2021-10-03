b6d9efee7b57d861cb23c7a83b356e335952b672
tomas-20 <tacuna20@stuy.edu>
2021-10-03 19:12:15 -0400

    added euler5(int)

--- euler.c
+++ euler.c
@@ -18,8 +18,14 @@ int gcf(int a, int b) {
 int lcm(int a, int b) {
   return a * b / gcf(a, b);
 }
+int euler5(int n) {
+  int total_lcm = 1;
+  for (int i = 2; i < n; i ++) {
+    total_lcm = lcm(total_lcm, i);
+  }
+  return total_lcm;
+}
 int main() {
   printf("euler1(1000)=%d\n", euler1(1000));
-  printf("%d\n", gcf(15, 40));
-  printf("%d\n", gcf(40, 15));
+  printf("euler5(20)=%d\n", euler5(20));
 }
