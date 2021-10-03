27a68b82946953428a732be15de56b781846d2b9
tomas-20 <tacuna20@stuy.edu>
2021-10-03 19:38:12 -0400

    added sum_of_squares(int) which shall be used in euler6(int)

--- euler.c
+++ euler.c
@@ -25,7 +25,18 @@ int euler5(int n) {
   }
   return total_lcm;
 }
+int sum_of_squares(int n) {
+  int square = 0;
+  int sum = 0;
+  int n2 = n * 2;
+  for (int i = 1; i <= n2; i += 2) {
+    square += i;
+    sum += square;
+  }
+  return sum;
+}
 int main() {
   printf("euler1(1000)=%d\n", euler1(1000));
   printf("euler5(20)=%d\n", euler5(20));
+  printf("sum_of_squares(10)=%d\n", sum_of_squares(10));
 }
