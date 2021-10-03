f103d586086ee19491e32fcc96e6ccce68c95d85
tomas-20 <tacuna20@stuy.edu>
2021-10-03 18:52:23 -0400

    added a gcf(int, int) function which shall be used in lcm(int, int)

--- euler.c
+++ euler.c
@@ -7,6 +7,16 @@ int euler1(int n) {
   int num = n - 1;
   return (factorsum(num, 3) + factorsum(num, 5) - factorsum(num, 15)) / 2;
 }
+int gcf(int a, int b) {
+  if (b == 0) {
+    return a;
+  }
+  else {
+    return gcf(b, a % b);
+  }
+}
 int main() {
   printf("euler1(1000)=%d\n", euler1(1000));
+  printf("%d\n", gcf(15, 40));
+  printf("%d\n", gcf(40, 15));
 }
